44d2c4f1d1ec484ada29cf750a03fa014ab6dc77
Kevin Galligan <kgalligan@gmail.com>
2024-04-29 13:25:13 -0400

    Import

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kmmbridge) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.cocoapods) apply false
+    alias(libs.plugins.sqlDelight) apply false
+    alias(libs.plugins.skie) apply false
+}
+
+subprojects {
+    val GROUP: String by project
+    val LIBRARY_VERSION: String by project
+    group = GROUP
+    version = LIBRARY_VERSION
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+include("breeds", "analytics", "allshared", ":testapps:android")
